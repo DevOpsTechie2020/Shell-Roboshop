1d8c8f58bd222121bc17ddc4ea6c1c005b01eaf9
Pavan Bokka <rampavan.bokka@gmail.com>
2025-08-24 12:49:07 +0530

    code added

--- Roboshop-Instances.sh
+++ Roboshop-Instances.sh
@@ -4,10 +4,10 @@ SG_ID="sg-0faec7651c2f9d221"   #replace with your Security Group ID
 INSTANCES=("MONGODB" "REDIS" "MYSQL" "RABBITMQ" "CATALOUGUE" "USER" "CART" "SHIPPING" "PAYMENT" "DISPATCH" "FRONTEND")
 ZONE_ID="Z08590672HICOEP27BESX" #replace with your Zone ID
 DOMAIN_NAME="dive2devops.com" #replace with yoyr domain
-for instances in ${INSTANCES[@]}
+for instance in ${INSTANCES[@]}
 do
-    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-group-ids sg-0faec7651c2f9d221 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instances}]" --query "Instance[0].InstanceId" --output text)
-    if [ $instances != "FRONTEND" ]
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-group-ids sg-0faec7651c2f9d221 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query "Instances[0].InstanceId" --output text)
+    if [ $instance != "FRONTEND" ]
     then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
         RECORD_NAME="$instances.$DOMAIN_NAME"
